6aba4b13e7df4464fe4372ae978b4a340f96d6aa
thadeus <thadeusnyariki94@gmail.com>
2022-10-20 19:22:28 +0300

    Done assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
 
+    attr_accessor :title, :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+b1 = Book.new("Then there we're few")
+p b1.title
+
+b1.author = "Rogers"
+p b1.author
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
 # Make your shoe class here!
+class Shoe
+
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+s1 = Shoe.new("Nike")
+p s1.brand
+
+s1.color = "white"
+p s1.color
